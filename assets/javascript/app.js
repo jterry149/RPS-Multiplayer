fd19131a689911440075c4518f6302a8d83c7ebc
JTerry <jessica.terry79@yahoo.com>
2018-10-01 18:39:14 -0500

    working on game functions in the app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,8 +9,43 @@ var config = {
 };
 
 firebase.initializeApp(config);
-// Global variables
 
-// Function to determine the winner
+// Global Variables
+var database = firebase.database(); // Variable to reference the firebase database
+var connected;                      // Hold connected to database
 
-// Function to start game play
+// player game object stored data
+var playerOne = {
+    number: "0",
+    name: "",
+    wins: 0,
+    losses: 0,
+    turns: 0,
+    choice: null
+};
+
+// opponent game object stored data
+var playerTwo ={
+    number: "0",
+    name: "",
+    wins: 0,
+    losses: 0,
+    turns: 0,
+    choice: null
+};
+
+// Variable for the assigned choices 
+var choices = ["rock", "paper", "scissors"];
+  
+// Functions to Display players choices
+var displayChoiceBtns = function() 
+{
+   
+    $("#playerOneChoices").html("<button id='rock'>Rock</button>"+"<br>"+ "<button id='paper'>Paper</button>"+"<br>"+ "<button id='scissors'>Scissors</button>")
+    
+    $("#playerTwoChoices").html("<button id='rock2'>Rock</button>"+"<br>"+ "<button id='paper2'>Paper</button>"+"<br>"+ "<button id='scissors2'>Scissors</button>")
+}
+
+// Call the functions
+displayChoiceBtns();
+  
